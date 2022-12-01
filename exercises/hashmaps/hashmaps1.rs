96bd967b9d77cab9b749aeb798ecdd523835f2ce
mazamu <506143016@qq.com>
2022-12-01 13:27:40 +0800

    hashmap and use of (e, function(e))

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,17 +10,22 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("banan"), 2);
+    basket.insert(String::from("bana"), 2);
+    basket.insert(String::from("ban"), 2);
+    basket.insert(String::from("banas"), 2);
+    basket.insert(String::from("ana"), 2);
+
 
     basket
 }
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(Debug)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -40,6 +40,40 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let count = scores.entry(team_1_name.clone())
+        .and_modify(
+            |e| {
+                e.goals_scored += team_1_score;
+                e.goals_conceded += team_2_score;
+            }
+        )
+        .or_insert(
+            Team{
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            }
+        );
+        // println!("{:?}", count);
+        // count.goals_scored += team_1_score;
+        // count.goals_conceded += team_2_score;
+        // println!("{:?}", count);
+        let count = scores.entry(team_2_name.clone())
+        .and_modify(
+            |e| {
+                e.goals_scored += team_2_score;
+                e.goals_conceded += team_1_score;
+            }
+        )
+        .or_insert(
+            Team{
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            }
+        );
+        // count.goals_scored += team_2_score;
+        // count.goals_conceded += team_1_score;
     }
     scores
 }
